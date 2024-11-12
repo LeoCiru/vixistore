34046e210cdb5b0efced28e3631571107f495af2
Leo Vásquez Vólquez <vasquezvolquezfelixleonel@gmail.com>
2024-11-12 19:49:47 -0400

    Creando y dando estilos iniciales al header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <main>
+      <Header/>
+      <hr />
+
+      <h1>Bienvenido a nuestra tienda</h1>
+    </main>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import { FaShoppingCart, FaRegHeart } from "react-icons/fa";
+
+function Header() {
+  return (
+    <header>
+        <nav>
+            <div className="first-part">
+                <h2>Vixi Store</h2>
+                
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Administrar</a></li>
+                </ul>
+            </div>
+
+            <div className="buttons-store">
+                <button className="cart">
+                    <FaShoppingCart />
+                </button>
+
+                <button className="wishlist">
+                    <FaRegHeart />
+                </button>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
